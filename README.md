a7b11f000b1e9268e8adb2cef8f85e00aa5fd6f1
Fabián Machuca <fabiam.machuca@algoritmo.co>
2023-10-12 13:13:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Instalar o clonar proyecto
+2. ```
+   npm i
+   ```
+3. Clonar elarchivo `.env.template` y renombrarlo a `.env`
+4. Ajustar la variables de entorno
+5. Levantar la BD:
+
+```
+docker compose up -d
+```
+
+6. Iniciar el API:
+
+```
+npm ru start:dev
+```
---
+++ src/products/products.service.ts
@@ -0,0 +1,61 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductsService'); // Se crea un instancia de Logger y se le indica que se va a utilizar el la clase ProductsService
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto); // Se crea la instancia del producto, no se graba en BD, por eso es síncrono
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async findAll() {
+    return await this.productRepository.find();
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+    // return await this.productRepository.delete({ id });
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
+  }
+}
