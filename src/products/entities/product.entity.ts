d29ba9d4ef27a741607030a9401ad9d17cb0c418
Fabián Machuca <fabiam.machuca@algoritmo.co>
2023-10-17 10:36:01 -0500

    Images (relations, query runners and transactions)

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from '.';
 
 @Entity()
 export class Product {
@@ -34,6 +36,12 @@ export class Product {
 
   @Column('text', { array: true, default: [] })
   tags: string[];
+
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+    eager: true, // Permite cargar los campos de la tabla relacionada
+  })
+  images?: ProductImage[];
 
   // Lógica para crear o modificar el slug antes de insertarlo:
   @BeforeInsert()
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,8 +9,8 @@ import { validate as isUUID } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
+import { DataSource, Repository } from 'typeorm';
+import { Product, ProductImage } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -19,13 +19,25 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRepository.create(createProductDto); // Se crea la instancia del producto, no se graba en BD, por eso es síncrono
-      await this.productRepository.save(product);
-      return product;
+      const { images = [], ...productDetails } = createProductDto;
+      const product = this.productRepository.create({
+        ...productDetails,
+        // Crear la instancia para las imágenes (tabla aparte)
+        images: images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        ),
+      }); // Se crea la instancia del producto, no se graba en BD, por eso es síncrono
+      await this.productRepository.save(product); // Guardar en ambas tablas
+      return { ...product, images };
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -33,10 +45,18 @@ export class ProductsService {
 
   async findAll(paginationDto) {
     const { limit = 10, offset = 0 } = paginationDto;
-    return await this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
+      relations: {
+        images: true,
+      },
     });
+
+    return products.map((product) => ({
+      ...product,
+      images: product.images.map((img) => img.url),
+    }));
   }
 
   async findOne(term: string) {
@@ -45,13 +65,14 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod'); // prod es un alias para la tabla product
       product = await queryBuilder
         // UPPER(title) pasa a mayúsculas el valor encontrado en BD
         .where('UPPER(title) =:title or slug =:slug', {
           title: term.toUpperCase(),
           slug: term.toLowerCase(),
         })
+        .leftJoinAndSelect('prod.images', 'prodImages') // prodImages es un alias para la tabla product_image
         .getOne();
     }
 
@@ -60,20 +81,49 @@ export class ProductsService {
     return product;
   }
 
+  // Este método se crea para devolver solo un array de imágenes y no un array de objetos.
+  // De esta forma se mantiene la respuesta del findOne como una instancia que pueda ser utilizada en otros métodos.
+  async findOnePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return { ...rest, images: images.map((image) => image.url) };
+  }
+
   async update(id: string, updateProductDto: UpdateProductDto) {
+    const { images, ...toUpdate } = updateProductDto;
+
     // preload busca por el primer parámetro y carga los campos asociados (segundo parámetro)
     const product = await this.productRepository.preload({
       id,
-      ...updateProductDto,
+      ...toUpdate,
     });
 
     if (!product)
       throw new NotFoundException(`Product with id ${id} not found`);
 
+    // Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      await this.productRepository.save(product);
-      return product;
+      if (images) {
+        // Eliminar las imágenes relacionadas con el productId en la tabla product_image:
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+
+        // Crear la instancia para las imágenes nuevas:
+        product.images = images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        );
+      }
+
+      await queryRunner.manager.save(product); // En este cason no se guarda, solo verifica que no haya errores
+      // await this.productRepository.save(product);
+      await queryRunner.commitTransaction(); // Guarda los cambios en BD
+      await queryRunner.release(); // finaliza la conexión del queryRunner
+      return this.findOnePlain(id);
     } catch (error) {
+      await queryRunner.rollbackTransaction(); // Devuelve los cambios si hay error
+      await queryRunner.release(); // finaliza la conexión del queryRunner
       this.handleDBExceptions(error);
     }
   }
@@ -90,5 +140,15 @@ export class ProductsService {
     throw new InternalServerErrorException(
       'Unexpected error, check server logs',
     );
+  }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 }
