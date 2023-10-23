3017dac8d592190db43771075287855ab811e070
Fabián Machuca <fabiam.machuca@algoritmo.co>
2023-10-23 16:55:41 -0500

    Upload and serve files

---
+++ src/files/files.controller.ts
@@ -0,0 +1,54 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter, fileNamer } from './helpers';
+import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response, // @Res() le quita el control a Nest para trabajar de forma 'manual', como Express.
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter, // En la documentación de Nest hacen la validación con ParseFilePipe
+      // limits: {fieldSize: 1000}
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) throw new BadRequestException('File must be an image');
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
+
+    return { secureUrl };
+  }
+}
